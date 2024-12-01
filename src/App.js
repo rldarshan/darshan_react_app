bc4752379d39c7dc73d89bcd704aa64b9b1004aa
rldarshan <rl.darshan01@gmail.com>
2024-12-01 22:08:38 +0530

    Added form-validation in App tab

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-import './styles.css';
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
   const [person, setPerson] = useState({
-    name: 'Niki de Saint Phalle',
+    name: "Niki de Saint Phalle",
     artwork: {
-      title: 'Blue Nana',
-      city: 'Hamburg',
-      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-    }
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
   });
-
+  
   function handleNameChange(e) {
     setPerson({
       ...person,
-      name: e.target.value
+      name: e.target.value,
     });
   }
 
@@ -23,8 +23,8 @@ export default function App() {
       ...person,
       artwork: {
         ...person.artwork,
-        title: e.target.value
-      }
+        title: e.target.value,
+      },
     });
   }
 
@@ -33,8 +33,8 @@ export default function App() {
       ...person,
       artwork: {
         ...person.artwork,
-        city: e.target.value
-      }
+        city: e.target.value,
+      },
     });
   }
 
@@ -43,51 +43,53 @@ export default function App() {
       ...person,
       artwork: {
         ...person.artwork,
-        image: e.target.value
-      }
+        image: e.target.value,
+      },
     });
   }
 
   return (
-    <div className='container d-grid justify-content-center'>
+    <div className="container d-grid justify-content-center">
       <br></br>
-      
-      <div className='form'>
-      <label>
-        Name:
-        <input
-          value={person.name}
-          onChange={handleNameChange}
-        />
-      </label>
-      <label>
-        Title:
-        <input
-          value={person.artwork.title}
-          onChange={handleTitleChange}
-        />
-      </label>
-      <label>
-        City:
-        <input
-          value={person.artwork.city}
-          onChange={handleCityChange}
-        />
-      </label>
-      <label>
-        Image:
-        <input
-          value={person.artwork.image}
-          onChange={handleImageChange}
-        />
-      </label>
+
+      <div className="form">
+        <div className="d-flex">
+          <label>
+            Name:
+            <input value={person.name} onChange={handleNameChange} />
+          </label>
+          {!person.name.trim().length && <span className="error">Name is required</span>}
+        </div>
+
+        <div className="d-flex">
+          <label>
+            Title:
+            <input value={person.artwork.title} onChange={handleTitleChange} />
+          </label>
+          {!person.artwork.title.trim().length && <span className="error">Title is required</span>}
+          </div>
+
+        <div className="d-flex">
+          <label>
+            City:
+            <input value={person.artwork.city} onChange={handleCityChange} />
+          </label>
+          {!person.artwork.city.trim().length && <span className="error">City is required</span>}
+          </div>
+
+        <div className="d-flex">
+          <label>
+            Image:
+            <input value={person.artwork.image} onChange={handleImageChange} />
+          </label>
+        </div>
       </div>
 
       <br></br>
       <div>
         <p>
           <i>{person.artwork.title}</i>
-          {' by '}
+          {" by "}
           {person.name}
           <br />
           (located in {person.artwork.city})
