73667861fb483164cc24ac7b7df06b0bbb4ced07
rldarshan <rl.darshan01@gmail.com>
2024-12-13 11:17:01 +0530

    Added cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'x8eim8',
+  projectId: "x8eim8",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
   },
 });
